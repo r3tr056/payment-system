f449a363f2dcc91c63d9c319b552c862c5bd00cc
hopeJRD <jordanh0pe@pm.me>
2023-06-19 22:26:05 +0530

    Moved 'server.js' to the src folder

---
+++ src/server.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+const shortid = require('shortid');
+const checksum = require("./lib/checksum");
+const { default: axios } = require('axios');
+const databaseConnection = require('./database')
+const { CreateChannel } = require('./utils')
+const customer_api = require('./api')
+
+const server = express();
+
+await databaseConnection();
+const channel = await CreateChannel()
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static(__dirname + '/public'));
+
+customer_api(app, channel);
+
+server.listen(config.SERVER_PORT, () => {
+    console.log(`Payment Processor Microservice is Listening on PORT : ${config.SERVER_PORT}`);
+}).on('error', (err) => {
+    console.log(err);
+    process.exit();
+}).on('close', () => {
+    channel.close();
+})
